3df57c0050ec6169cdaf43e1848555dc734a8833
ian <ianvictorandrade@hotmail.com>
2023-12-30 20:41:02 -0400

    adicionada nova página

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-
 async function buscarDadosApi(event) {
 
     event.preventDefault();
     try {
-
         const username = document.getElementById("input-user").value.toLowerCase();
         const response = await fetch(`https://api.github.com/users/${username}/repos`);
+
 
         if (!response.ok) {
             throw new Error("Não foi possível obter os dados da API");
@@ -14,7 +13,15 @@ async function buscarDadosApi(event) {
         const data = await response.json();
         console.log(data);
 
-    } catch (error) {
+        window.location.href = 'paginaRepositorios.html';
+
+
+    } catch
+        (error) {
         console.error(error);
     }
+}
+
+function voltar() {
+    window.location.href = 'index.html';
 }
