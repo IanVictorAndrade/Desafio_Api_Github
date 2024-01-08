b00b36a95e4c80fedcc75bad678e55101d82d029
ianvictor <ianvictorandrade@hotmail.com>
2024-01-08 09:03:22 -0400

    alterações na lógica do javascript, criando outro metodo e adicionando um event listener

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
-async function buscarDadosApi(event) {
+async function pegarUsername(event) {
 
     event.preventDefault();
-    try {
-        const username = document.getElementById("input-user").value.toLowerCase();
-        const response = await fetch(`https://api.github.com/users/${username}/repos`);
+    const username = document.getElementById("input-user").value.toLowerCase();
+    const response = await fetch(`https://api.github.com/users/${username}/repos`)
 
-
-        if (!response.ok) {
-            throw new Error("Não foi possível obter os dados da API");
-        }
-
-        const data = await response.json();
-        console.log(data);
-
-        window.location.href = 'paginaRepositorios.html';
-
-
-    } catch
-        (error) {
-        console.error(error);
+    if (response.status === 200) {
+    window.location.href = 'paginaRepositorios.html?username=' + username;
+    } else {
+        alert("Usuário não encontrado!");
     }
+
 }
 
 function voltar() {
     window.location.href = 'index.html';
 }
+
+
+document.addEventListener("DOMContentLoaded", async function() {
+    let params = new URLSearchParams(window.location.search);
+    let username = params.get("username");
+    
+    await fetch(`https://api.github.com/users/${username}/repos`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.error('Erro:', error);
+        });
+ });
+ 
